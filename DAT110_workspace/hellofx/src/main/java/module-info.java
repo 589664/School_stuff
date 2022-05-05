16f669e3adf8c7b859cefaad348e14a7d7b897a9
Maksim Ohvrill <589664@stud.hvl.no>
2022-05-05 18:36:55 +0200

    final commit

--- DAT110_workspace/hellofx/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.openjfx.hellofx {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires okhttp3;
-	requires com.google.gson;
-
-    opens org.openjfx.hellofx to javafx.fxml;
-    exports org.openjfx.hellofx;
-}
